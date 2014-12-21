8c87c9c049d8ee5e9b37c76ba933a03b9181bdd6
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2014-12-22 00:02:09 +0100

    finish array and equality testing exercise

--- lib/array/kitchen.rb
+++ lib/array/kitchen.rb
@@ -8,4 +8,42 @@ class Kitchen
   def items
     @items
   end
+
+  def add(item)
+    @items << item
+  end
+
+  def remove_first_and_last_elements
+    @items.delete_at(0)
+    @items.pop
+  end
+
+  def add_bang
+    @items.map!{|item| item + "!"}
+  end
+
+  def remove_with_slice(items)
+    kitchen_array = []
+    items.each do |item|
+      kitchen_array << item unless item == "rat" || item == "shoe"
+    end
+    kitchen_array
+  end
+
+  def make_mini_arrays(items)
+    items.combination(1).to_a
+  end
+
+  def remove_if_more_than_three_letters(items)
+    items.reject{|item| item.length > 3}
+  end
+
+  def alphabetically_backwards(items)
+    items.sort.reverse
+  end
+
+  def first_three(items)
+    items.slice(0,3)
+  end
+
 end
--- lib/equality/equality.rb
+++ lib/equality/equality.rb
@@ -1,3 +1,10 @@
+require 'set'
 class Equality
- 
+ def are_sorted_the_same(a,b)
+  a.sort == b.sort
+ end
+
+ def are_of_the_same_set(a,b)
+  a.to_set == b.to_set
+ end
 end
